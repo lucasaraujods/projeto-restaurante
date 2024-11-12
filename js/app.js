6d58a48ec58f5fffa740b4015f64983fba4a8596
Lucas Araujo <lucasara1707@gmail.com>
2024-11-12 17:33:47 -0300

    subindo arquivos de manipulações de JS

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//importanto o jquery
+//a partir do documento ser totalmente lido ele vai executar uma função
+$(document).ready(function () {
+    cardapio.eventos.init()
+})
+
+//criando um váriavel sendo orinetado ao objeto, para ficar mais fácil de se situar
+var cardapio = {};
+
+cardapio.eventos = {
+    //criando uma função para quando inicializar
+    init: () => {
+        console.log("iniciou")
+    }
+
+}
+
+cardapio.metodos = {
+
+}
+
+cardapio.templates = {
+
+}
