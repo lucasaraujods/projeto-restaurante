976b2207873eef6e57b019315ad9e758d9c7244f
Lucas Araujo <lucasara1707@gmail.com>
2024-11-15 09:26:40 -0300

    aiiconado funcionalidade nos botões de adicionar quantidade do item

--- js/app.js
+++ js/app.js
@@ -17,13 +17,19 @@ cardapio.eventos = {
 
 cardapio.metodos = {
     //para listar os itens, obtendo a lista de itens do cardapio 
-    obterItensCardapio: (categoria = 'burgers') => {
+    obterItensCardapio: (categoria = 'burgers', vermais=false) => {
 
         let filtro = MENU[categoria]
         console.log(filtro)
 
-        //limpando a tela para depois dar o append
-        $("#itensCardapio").html('')
+         //limpando a tela para depois dar o append
+        if(!vermais){
+            $("#itensCardapio").html('')
+            $("#btnVerMais").removeClass("hidden")
+        }
+
+       
+       
 
         //*criando um for each em jquery para obter os 12 produtos e cada vez que e a repetição ele cria um template
         //o e é o elemento assim segue os 12 itens do cardápio do array
@@ -34,9 +40,17 @@ cardapio.metodos = {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${name}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.',','))
-            //assim usamos o id para jogar para página e adicionameos o temp
-            $("#itensCardapio").append(temp)
+            .replace(/\${id}/g, e.id)
 
+            //ao clicar no ver mais exbir os 4 ultimos itens, assim total =12 itens
+            if(vermais && i >=8 && i < 12){
+                $("#itensCardapio").append(temp)
+            }
+            //paginação inicial = 8 itens
+            if(!vermais && i < 8){
+                $("#itensCardapio").append(temp)
+            }
+          
         })
 
         //removendo o ativo para o click, ou seja, a parte amarelha deve ser removida o active
@@ -44,14 +58,42 @@ cardapio.metodos = {
 
         // coloca o menu para ativo
         $('#menu-' + categoria).addClass('active')
-    }
+    },
+
+    verMais: () => {
+
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1] //menu burgers, assim o split quebra o array e pegamso o nome no array 1
+        cardapio.metodos.obterItensCardapio(ativo,true)
+
+        $("#btnVerMais").addClass('hidden')
+    },
+
+    //diminuir a quantidade do item no cardapio
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual =parseInt($("#qntd-" + id).text()) 
+
+        if(qntdAtual > 0){
+
+            $("#qntd-" + id).text(qntdAtual-1)
+
+        }
+    },
+
+    //aumentar a quantidade do item no cardapio
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual =parseInt($("#qntd-" + id).text())
+
+        $("#qntd-" + id).text(qntdAtual+1)
+    },
 }
 
 cardapio.templates = {
 
     item: `                     
         <div class="col-3 mb-5">
-            <div class="card card-item">
+            <div class="card card-item" id="\${id}">
 
                 <div class="img-produto">
                     <img src="\${img}">
@@ -67,9 +109,9 @@ cardapio.templates = {
 
                 <!--testenado botões de mais e menos para adicionar o produto no carrinho-->
                 <div class="add-carrinho">
-                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
-                    <span class="add-numero-itens">0</span>
-                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
+                    <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" onClick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
                     <span class="btn btn-add"><i class="fas fa-shopping-bag"></i></span>
                 </div>
             </div>
